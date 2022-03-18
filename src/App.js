435d8f4cb3307d8f84149e8d724708711295660f
csaw1510 <alexander.haele@student.uibk.ac.at>
2022-03-18 18:15:31 +0100

    added catch function(not yet with table) + updated API access

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { useState } from "react";
 import PokemonAPI, { getPokemonByNameAPI, getPokemonListAPI } from './PokemonAPI'
 
 //from https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-
 function RenderTableHeader(props){
   if(props.headerArray){
     if(props.headerArray.length>0){
@@ -45,30 +46,42 @@ function RenderTable(props){
 }
 
 function RenderSpawnedPokemon(props){
-  console.log(props);
-  if(props.hasSpawned){
+  if(props.hasSpawned && props.pokemonPicUrl && props.pokemonName){
     return(
+      <div>
       <h1> {props.pokemonName} </h1>
+      <img src={props.pokemonPicUrl} />
+      </div>
     );
   }
   return null;
 }
 
 const App = () => {
+  const [numberOfPokemon, setNumberOfPokemon] = useState(899);
   const [pokemonName, setPokemonName] = useState("pikachu");
-  const [pokemonStats, setPokemonStats] = useState([{pokemon: '', hp: null, attack: null, defense: null, "special-attack": null , "special-defense": null, speed: null}])
+  const [pokemonPicUrl, setPokemonPicUrl] = useState("");
+  const [pokemonStats, setPokemonStats] = useState([{pokemon: '', hp: null, attack: null, defense: null, "special-attack": null , "special-defense": null, speed: null}]);
   const [hasSpawned, setHasSpawned] = useState(false);
+  const [catchedPokemons, setCatchedPokemons] = useState(new Set());
   
   const getPokemon = async () => {
-    getPokemonListAPI().then(function(results){
-      let randomID = getRandomInt(results.length);
-      setPokemonName(results[randomID].name);
-    });
-
-    getPokemonByNameAPI(pokemonName).then(function(results){
-      setPokemonStats(results.stats)
+    let randomID = getRandomInt(1, numberOfPokemon);
+    console.log(randomID);
+    getPokemonByNameAPI(randomID).then(function(results){
+      console.log(results);
+      setPokemonName(results.name);
+      setPokemonStats(results.stats);
+      setPokemonPicUrl(results.sprites["front_default"]);
     });
     setHasSpawned(true);
+  }
+
+  const catchPokemon = () => {
+      let catchedPokemon = catchedPokemons; 
+      catchedPokemon.add(pokemonName);
+      setCatchedPokemons(catchedPokemon);
+      console.log(catchedPokemons)        
   }
 
   return (
@@ -78,7 +91,11 @@ const App = () => {
         Spawn Pokemon
         </button>
 
-        <RenderSpawnedPokemon hasSpawned={hasSpawned} pokemonName={pokemonName}> </RenderSpawnedPokemon>
+        <button onClick={catchPokemon} >
+          Catch Pokemon
+        </button>
+
+        <RenderSpawnedPokemon hasSpawned={hasSpawned} pokemonName={pokemonName} pokemonPicUrl ={pokemonPicUrl} >  </RenderSpawnedPokemon>
       </div>
       <div>
       <RenderTable headerArray={["Pokemon", "HP", "Attack", "Defense", "Special-attack", "Special-defense", "Speed"]}></RenderTable>
